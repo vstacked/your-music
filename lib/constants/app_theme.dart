09c319c822b06f636f83a60bb1f025b6d66e23a6
rizki <54944887+vstacked@users.noreply.github.com>
2021-09-19 06:05:04 +0700

    Add constants

---
+++ lib/constants/app_theme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:your_music/constants/colors.dart';
+
+final darkTheme = ThemeData(
+  brightness: Brightness.dark,
+  primaryColor: primaryColor,
+  accentColor: secondaryColor,
+  scaffoldBackgroundColor: secondaryColor,
+);
---
+++ lib/constants/colors.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+const primaryColor = Color(0xFF1a1b26);
+const secondaryColor = Color(0xFF24283b);
+
+const blueColor = Color(0xFF34548a);
+const greyColor = Color(0xFFcfc9c2);
+
+const Map<int, Color> swatch = {
+  50: secondaryColor,
+  100: secondaryColor,
+  200: secondaryColor,
+  300: secondaryColor,
+  400: secondaryColor,
+  500: secondaryColor,
+  600: secondaryColor,
+  700: secondaryColor,
+  800: secondaryColor,
+  900: secondaryColor,
+};
