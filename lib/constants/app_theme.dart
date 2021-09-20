791fdb4f3ba6cbaaac78131ec730e33b31661be0
rizki <54944887+vstacked@users.noreply.github.com>
2021-09-21 01:37:55 +0700

    Add constants & utils

--- lib/constants/app_theme.dart
+++ lib/constants/app_theme.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:your_music/constants/colors.dart';
+import 'package:your_music/utils/color/generate_material_color.dart';
 
 final darkTheme = ThemeData(
   brightness: Brightness.dark,
   primaryColor: primaryColor,
   accentColor: secondaryColor,
   scaffoldBackgroundColor: secondaryColor,
+  primarySwatch: generateMaterialColor(greyColor),
 );
--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -5,16 +5,4 @@ const secondaryColor = Color(0xFF24283b);
 
 const blueColor = Color(0xFF34548a);
 const greyColor = Color(0xFFcfc9c2);
-
-const Map<int, Color> swatch = {
-  50: secondaryColor,
-  100: secondaryColor,
-  200: secondaryColor,
-  300: secondaryColor,
-  400: secondaryColor,
-  500: secondaryColor,
-  600: secondaryColor,
-  700: secondaryColor,
-  800: secondaryColor,
-  900: secondaryColor,
-};
+const redColor = Color(0xFF8c4351);
