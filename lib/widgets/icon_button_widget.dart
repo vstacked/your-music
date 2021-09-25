064e5b59c480955136258d9b50a2813ee079eac9
rizki <54944887+vstacked@users.noreply.github.com>
2021-09-26 04:54:35 +0700

    Add outlinedButton

--- lib/widgets/icon_button_widget.dart
+++ lib/widgets/icon_button_widget.dart
@@ -1,17 +1,38 @@
 import 'package:flutter/material.dart';
 
 class IconButtonWidget extends StatelessWidget {
-  const IconButtonWidget({Key? key, required this.icon, required this.color, required this.onPressed})
-      : super(key: key);
+  const IconButtonWidget({
+    Key? key,
+    required this.icon,
+    required this.color,
+    required this.onPressed,
+    this.isOutlinedButton = false,
+    this.buttonText = '',
+  }) : super(key: key);
+
   final Icon icon;
   final VoidCallback onPressed;
   final Color color;
+  final bool isOutlinedButton;
+  final String buttonText;
 
   @override
   Widget build(BuildContext context) {
-    return Transform.scale(
-      scale: .75,
-      child: IconButton(onPressed: onPressed, icon: icon, color: color, iconSize: 40, padding: const EdgeInsets.all(2)),
-    );
+    if (isOutlinedButton) {
+      return OutlinedButton.icon(
+        onPressed: onPressed,
+        icon: Text(buttonText),
+        label: icon,
+        style: ButtonStyle(
+          foregroundColor: MaterialStateProperty.all(color),
+          side: MaterialStateProperty.all(BorderSide(color: color)),
+          fixedSize: MaterialStateProperty.all(const Size.fromHeight(47.5)),
+          shape: MaterialStateProperty.all(RoundedRectangleBorder(borderRadius: BorderRadius.circular(10))),
+          textStyle: MaterialStateProperty.all(Theme.of(context).textTheme.subtitle1),
+        ),
+      );
+    }
+
+    return IconButton(onPressed: onPressed, icon: icon, color: color, iconSize: 35, splashRadius: 30);
   }
 }
