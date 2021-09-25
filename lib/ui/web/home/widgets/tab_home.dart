ac07c6f45f22f1ebcac091c378565445b2ee9c91
rizki <54944887+vstacked@users.noreply.github.com>
2021-09-26 05:28:38 +0700

    Refactor, Add delete checkbox

--- lib/ui/web/home/widgets/tab_home.dart
+++ lib/ui/web/home/widgets/tab_home.dart
@@ -5,9 +5,15 @@ import 'package:your_music/widgets/responsive_layout.dart';
 
 import '../home.dart';
 
-class TabHome extends StatelessWidget {
+class TabHome extends StatefulWidget {
   const TabHome({Key? key}) : super(key: key);
 
+  @override
+  _TabHomeState createState() => _TabHomeState();
+}
+
+class _TabHomeState extends State<TabHome> {
+  bool isOutlinedDelete = true;
   @override
   Widget build(BuildContext context) {
     final textTheme = Theme.of(context).textTheme;
@@ -15,28 +21,7 @@ class TabHome extends StatelessWidget {
       quarterTurns: -1,
       child: Column(
         children: <Widget>[
-          if (ResponsiveLayout.isSmallScreen(context))
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 35),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  IconButtonWidget(
-                    icon: const Icon(Icons.menu),
-                    color: greyColor,
-                    onPressed: () => scaffoldKey.currentState!.openDrawer(),
-                  ),
-                  Text(
-                    'Your Music',
-                    style:
-                        textTheme.headline5!.copyWith(color: greyColor.withOpacity(.75), fontWeight: FontWeight.w600),
-                  ),
-                  const SizedBox(width: 25),
-                ],
-              ),
-            )
-          else
-            const SizedBox(height: 75),
+          const _AppBar(),
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 25),
             child: Row(
@@ -47,9 +32,15 @@ class TabHome extends StatelessWidget {
                   mainAxisSize: MainAxisSize.min,
                   children: [
                     IconButtonWidget(
-                      icon: const Icon(Icons.delete),
+                      icon: const Icon(Icons.delete, size: 35),
+                      buttonText: 'Remove Selected',
+                      isOutlinedButton: isOutlinedDelete,
                       color: redColor,
-                      onPressed: () {},
+                      onPressed: () {
+                        setState(() {
+                          isOutlinedDelete = !isOutlinedDelete;
+                        });
+                      },
                     ),
                     const SizedBox(width: 10),
                     IconButtonWidget(
@@ -63,36 +54,112 @@ class TabHome extends StatelessWidget {
             ),
           ),
           const SizedBox(height: 13.5),
-          Flexible(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: ScrollConfiguration(
-                behavior: ScrollConfiguration.of(context).copyWith(scrollbars: false),
-                child: GridView.builder(
-                  shrinkWrap: true,
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: ResponsiveLayout.isMediumScreen(context)
-                        ? 6
-                        : (ResponsiveLayout.isSmallScreen(context) ? 4 : 7),
-                  ),
-                  itemCount: 100,
-                  itemBuilder: (context, index) => Padding(
-                    padding: const EdgeInsets.all(5),
-                    child: DecoratedBox(
-                      decoration: BoxDecoration(
-                        image: const DecorationImage(
-                          image: NetworkImage('https://placeimg.com/640/480/business'),
-                          fit: BoxFit.cover,
-                        ),
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                    ),
+          _GridItems(isCheckbox: isOutlinedDelete),
+        ],
+      ),
+    );
+  }
+}
+
+class _AppBar extends StatelessWidget {
+  const _AppBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+    if (ResponsiveLayout.isSmallScreen(context)) {
+      return Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 35),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            IconButtonWidget(
+              icon: const Icon(Icons.menu),
+              color: greyColor,
+              onPressed: () => scaffoldKey.currentState!.openDrawer(),
+            ),
+            Text(
+              'Your Music',
+              style: textTheme.headline5!.copyWith(color: greyColor.withOpacity(.75), fontWeight: FontWeight.w600),
+            ),
+            const SizedBox(width: 25),
+          ],
+        ),
+      );
+    }
+    return const SizedBox(height: 75);
+  }
+}
+
+class _GridItems extends StatelessWidget {
+  const _GridItems({Key? key, required this.isCheckbox}) : super(key: key);
+  final bool isCheckbox;
+
+  @override
+  Widget build(BuildContext context) {
+    return Flexible(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: ScrollConfiguration(
+          behavior: ScrollConfiguration.of(context).copyWith(scrollbars: false),
+          child: GridView.builder(
+            shrinkWrap: true,
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount:
+                  ResponsiveLayout.isMediumScreen(context) ? 6 : (ResponsiveLayout.isSmallScreen(context) ? 4 : 7),
+            ),
+            itemCount: 100,
+            itemBuilder: (context, index) {
+              return _Item(isCheckbox: isCheckbox);
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _Item extends StatefulWidget {
+  const _Item({Key? key, required this.isCheckbox}) : super(key: key);
+  final bool isCheckbox;
+
+  @override
+  __ItemState createState() => __ItemState();
+}
+
+class __ItemState extends State<_Item> {
+  bool isSelected = false;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(5),
+      child: GestureDetector(
+        onTap: widget.isCheckbox ? () => setState(() => isSelected = !isSelected) : null,
+        child: DecoratedBox(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: const NetworkImage('https://placeimg.com/640/480/business'),
+              fit: BoxFit.cover,
+              colorFilter: widget.isCheckbox ? ColorFilter.mode(overlayColor, BlendMode.multiply) : null,
+            ),
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: Builder(
+            builder: (_) {
+              if (!widget.isCheckbox) return const SizedBox();
+              return Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Align(
+                  alignment: Alignment.topRight,
+                  child: Checkbox(
+                    value: isSelected,
+                    onChanged: (_) => setState(() => isSelected = !isSelected),
                   ),
                 ),
-              ),
-            ),
-          )
-        ],
+              );
+            },
+          ),
+        ),
       ),
     );
   }
