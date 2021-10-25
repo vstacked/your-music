7f95983be8183972baf20a7707b65961d19ac158
rizki <54944887+vstacked@users.noreply.github.com>
2021-10-26 03:07:44 +0700

    Add method login, update & delete song

--- lib/data/services/firebase_service.dart
+++ lib/data/services/firebase_service.dart
@@ -1,5 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:your_music/models/song_model.dart';
 
@@ -7,34 +9,105 @@ class FirebaseService {
   FirebaseService._();
   static final instance = FirebaseService._();
 
+  final FirebaseAuth _auth = FirebaseAuth.instance;
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
   final FirebaseStorage _storage = FirebaseStorage.instance;
 
-  Future<Map<String, dynamic>> adminAuth() async {
+  Future<Map<String, dynamic>> _adminAuth() async {
     final data = await _firestore.collection('admin').get();
     if (data.docs.isNotEmpty) return data.docs.first.data();
     return {};
   }
 
-  Future<bool> saveSong(SongModel songModel) async {
+  bool get isLogged => _auth.currentUser != null;
+
+  Future<bool> login(String username, String password) async {
+    try {
+      final data = await _adminAuth();
+      if (data.isEmpty) throw Exception('Error Auth');
+
+      if (username == data['username'] && password == data['password']) {
+        await _auth.signInAnonymously();
+        return true;
+      }
+
+      return false;
+    } catch (e) {
+      debugPrint('$e');
+      return false;
+    }
+  }
+
+  Future<bool> saveOrUpdateSong(SongModel songModel, {bool isEdit = false}) async {
     try {
       final _collection = _firestore.collection('songs');
-      final song =
-          await _storage.ref('songs/${songModel.songPlatformFile!.name}').putData(songModel.songPlatformFile!.bytes!);
-      final thumbnail = await _storage
-          .ref('thumbnails/${songModel.thumbnailPlatformFile!.name}')
-          .putData(songModel.thumbnailPlatformFile!.bytes!);
 
-      await _collection.add({
-        'title': songModel.title,
-        'singer': songModel.singer,
-        'song': await song.ref.getDownloadURL(),
-        'thumbnail': await thumbnail.ref.getDownloadURL(),
-        'lyric': songModel.lyric ?? '',
-        'description': songModel.description ?? '',
-        'created_at': DateTime.now().toLocal(),
-        'active': true,
-      });
+      final _template = '${_auth.currentUser!.uid}-${DateTime.now().toLocal().toIso8601String()}';
+
+      if (!isEdit) {
+        final song = await _storage
+            .ref('songs/$_template.${songModel.songPlatformFile!.name.split('.').last}')
+            .putData(songModel.songPlatformFile!.bytes!);
+
+        final thumbnail = await _storage
+            .ref('thumbnails/$_template.${songModel.thumbnailPlatformFile!.name.split('.').last}')
+            .putData(songModel.thumbnailPlatformFile!.bytes!);
+
+        await _collection.add({
+          'title': songModel.title,
+          'singer': songModel.singer,
+          'song': {
+            'name': songModel.songPlatformFile!.name,
+            'url': await song.ref.getDownloadURL(),
+          },
+          'thumbnail_url': await thumbnail.ref.getDownloadURL(),
+          'lyric': songModel.lyric ?? '',
+          'description': songModel.description ?? '',
+          'created_at': DateTime.now().toLocal(),
+          'active': true,
+        });
+      } else {
+        await _collection.doc(songModel.id).update({
+          'title': songModel.title,
+          'singer': songModel.singer,
+          'lyric': songModel.lyric ?? '',
+          'description': songModel.description ?? '',
+        });
+
+        if (songModel.songPlatformFile != null) {
+          final song = await _storage
+              .ref('songs/$_template.${songModel.songPlatformFile!.name.split('.').last}')
+              .putData(songModel.songPlatformFile!.bytes!);
+
+          await _collection.doc(songModel.id).update({
+            'song': {
+              'name': songModel.songPlatformFile!.name,
+              'url': await song.ref.getDownloadURL(),
+            }
+          });
+        }
+
+        if (songModel.thumbnailPlatformFile != null) {
+          final thumbnail = await _storage
+              .ref('thumbnails/$_template.${songModel.thumbnailPlatformFile!.name.split('.').last}')
+              .putData(songModel.thumbnailPlatformFile!.bytes!);
+
+          await _collection.doc(songModel.id).update({
+            'thumbnail_url': await thumbnail.ref.getDownloadURL(),
+          });
+        }
+      }
+
+      return true;
+    } catch (e) {
+      debugPrint(e.toString());
+      return false;
+    }
+  }
+
+  Future<bool> deleteSong(String id) async {
+    try {
+      await _firestore.collection('songs').doc(id).update({'active': false});
       return true;
     } catch (e) {
       debugPrint(e.toString());
