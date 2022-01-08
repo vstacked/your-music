ac335226d01e176eb815624fe7986a8484709cb0
rizki <54944887+vstacked@users.noreply.github.com>
2022-01-09 03:03:03 +0700

    feat: :sparkles: setup hive for platform android

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,16 @@
 import 'dart:async';
+import 'dart:io';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:hive/hive.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:url_strategy/url_strategy.dart';
 
 import 'constants/colors.dart';
+import 'models/favorite_model.dart';
 import 'ui/my_app.dart';
 
 DotEnv dotenv = DotEnv();
@@ -14,6 +18,14 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await setPreferredOrientations();
   await dotenv.load(fileName: '.env');
+
+  if (Platform.isAndroid) {
+    final directory = await getApplicationDocumentsDirectory();
+    Hive.init(directory.path);
+    Hive.registerAdapter(FavoriteModelAdapter());
+    await Hive.openBox<FavoriteModel>('favorites');
+  }
+
   setPathUrlStrategy();
   return runZonedGuarded(
     () async {
