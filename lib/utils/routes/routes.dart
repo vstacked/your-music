6737567a3b42b127d4ecbc5775de79b5bd76f924
rizki <54944887+vstacked@users.noreply.github.com>
2022-01-24 02:45:44 +0700

    feat(routes): :sparkles: add now-playing route

--- lib/utils/routes/routes.dart
+++ lib/utils/routes/routes.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 
 import '../../ui/android/home/favorite.dart';
 import '../../ui/android/home/home.dart' as android;
+import '../../ui/android/now_playing/now_playing.dart';
 import '../../ui/web/home/home.dart' as web;
 import '../../ui/web/login/login.dart';
 
@@ -12,10 +13,12 @@ class Routes {
   static const String home = '/';
   static const String login = '/login';
   static const String favorite = '/favorite';
+  static const String nowPlaying = '/now-playing';
 
   static final routes = <String, WidgetBuilder>{
     home: (BuildContext context) => (kIsWeb) ? const web.Home() : const android.Home(),
     login: (BuildContext context) => const Login(),
     favorite: (BuildContext context) => const Favorite(),
+    nowPlaying: (BuildContext context) => const NowPlaying(),
   };
 }
